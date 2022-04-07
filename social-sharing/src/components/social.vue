466ed55d1ddcca7851971df2f40267537a4e364b
wondercrazy15 <wondercrazy15@gmail.com>
2022-04-07 12:50:38 +0530

    vuejs demos commit

--- social-sharing/src/components/social.vue
+++ social-sharing/src/components/social.vue
@@ -16,28 +16,13 @@
     :hashtags="sharing.hashtags"
     :twitterUser="sharing.twitterUser"
   >
-   <!-- :style="{backgroundColor: network.color}" -->
+  
   <div class="social-data" :style="{backgroundColor: network.color}" >
      <i :class="network.icon"></i>
     <span>{{ network.name }}</span>
   </div>
    
   </ShareNetwork>
-
-  <!-- <div>
-      <network network="email">
-          <i class="fa fa-envelope"></i> Email
-      </network>
-      <network network="facebook">
-        <i class="fa fa-facebook"></i> Facebook
-      </network>
-      <network network="googleplus">
-        <i class="fa fa-google-plus"></i> Google +
-      </network>
-      <network network="line">
-        <i class="fa fa-line"></i> Line
-      </network>
-  </div> -->
 </div>
 </template>
 
@@ -54,36 +39,13 @@ export default {
         twitterUser: 'youyuxi'
       },
       networks: [
-        // { network: 'baidu', name: 'Baidu', icon: 'fas fah fa-lg fa-paw', color: '#2529d8' },
-        // { network: 'buffer', name: 'Buffer', icon: 'fab fah fa-lg fa-buffer', color: '#323b43' },
+     
         { network: 'email', name: 'Email', icon: 'far fah fa-lg fa-envelope', color: '#333333' },
-        // { network: 'evernote', name: 'Evernote', icon: 'fab fah fa-lg fa-evernote', color: '#2dbe60' },
         { network: 'facebook', name: 'Facebook', icon: 'fab fah fa-lg fa-facebook-f', color: '#1877f2' },
-        // { network: 'flipboard', name: 'Flipboard', icon: 'fab fah fa-lg fa-flipboard', color: '#e12828' },
-        // { network: 'hackernews', name: 'HackerNews', icon: 'fab fah fa-lg fa-hacker-news', color: '#ff4000' },
-        // { network: 'instapaper', name: 'Instapaper', icon: 'fas fah fa-lg fa-italic', color: '#428bca' },
-        // { network: 'line', name: 'Line', icon: 'fab fah fa-lg fa-line', color: '#00c300' },
         { network: 'linkedin', name: 'LinkedIn', icon: 'fab fah fa-lg fa-linkedin', color: '#007bb5' },
-        // { network: 'messenger', name: 'Messenger', icon: 'fab fah fa-lg fa-facebook-messenger', color: '#0084ff' },
-        // { network: 'odnoklassniki', name: 'Odnoklassniki', icon: 'fab fah fa-lg fa-odnoklassniki', color: '#ed812b' },
-        // { network: 'pinterest', name: 'Pinterest', icon: 'fab fah fa-lg fa-pinterest', color: '#bd081c' },
-        // { network: 'pocket', name: 'Pocket', icon: 'fab fah fa-lg fa-get-pocket', color: '#ef4056' },
-        // { network: 'quora', name: 'Quora', icon: 'fab fah fa-lg fa-quora', color: '#a82400' },
-        // { network: 'reddit', name: 'Reddit', icon: 'fab fah fa-lg fa-reddit-alien', color: '#ff4500' },
-        // { network: 'skype', name: 'Skype', icon: 'fab fah fa-lg fa-skype', color: '#00aff0' },
-        // { network: 'sms', name: 'SMS', icon: 'far fah fa-lg fa-comment-dots', color: '#333333' },
-        // { network: 'stumbleupon', name: 'StumbleUpon', icon: 'fab fah fa-lg fa-stumbleupon', color: '#eb4924' },
         { network: 'telegram', name: 'Telegram', icon: 'fab fah fa-lg fa-telegram-plane', color: '#0088cc' },
-        // { network: 'tumblr', name: 'Tumblr', icon: 'fab fah fa-lg fa-tumblr', color: '#35465c' },
-        // { network: 'twitter', name: 'Twitter', icon: 'fab fah fa-lg fa-twitter', color: '#1da1f2' },
-        // { network: 'viber', name: 'Viber', icon: 'fab fah fa-lg fa-viber', color: '#59267c' },
-        // { network: 'vk', name: 'Vk', icon: 'fab fah fa-lg fa-vk', color: '#4a76a8' },
-        // { network: 'weibo', name: 'Weibo', icon: 'fab fah fa-lg fa-weibo', color: '#e9152d' },
         { network: 'whatsapp', name: 'Whatsapp', icon: 'fab fah fa-lg fa-whatsapp', color: '#25d366' }
-        // { network: 'wordpress', name: 'Wordpress', icon: 'fab fah fa-lg fa-wordpress', color: '#21759b' },
-        // { network: 'xing', name: 'Xing', icon: 'fab fah fa-lg fa-xing', color: '#026466' },
-        // { network: 'yammer', name: 'Yammer', icon: 'fab fah fa-lg fa-yammer', color: '#0072c6' },
-        // { network: 'fakeblock', name: 'Custom Network', icon: 'fab fah fa-lg fa-vuejs', color: '#41b883' }
+
       ]
     }
   }
@@ -99,6 +61,5 @@ a:link {
   height: 30px;
   width: 100px;
   margin: 10px;
-  /* padding: 5px; */
 }
 </style>
--- social-sharing/src/main.js
+++ social-sharing/src/main.js
@@ -4,26 +4,4 @@ import App from './App.vue'
 import VueSocialSharing from 'vue-social-sharing'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-// Vue.use(VueSocialSharing, {
-//     networks: {
-//       fakeblock: 'https://fakeblock.com/share?url=@url&title=@title'
-//     }
-//   })
 createApp(App).use(VueSocialSharing).mount('#app')
-
-
-// import Vue from 'vue'
-// import VueSocialSharing from '@/vue-social-sharing'
-
-// import App from './App.vue'
-
-// Vue.use(VueSocialSharing, {
-//   networks: {
-//     fakeblock: 'https://fakeblock.com/share?url=@url&title=@title'
-//   }
-// })
-
-// new Vue({
-//   el: '#app',
-//   render: createElement => createElement(App)
-// })
