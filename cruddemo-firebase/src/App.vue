ae4e5bcbdaefa2a7fdb1e8f4a97d3e1217cd6ebd
wondercrazy15 <wondercrazy15@gmail.com>
2022-04-07 12:43:33 +0530

    vuejs demos commit

--- cruddemo-firebase/src/App.vue
+++ cruddemo-firebase/src/App.vue
@@ -27,9 +27,7 @@
 <script>
 export default {
   name: 'App',
-  // components:{
-  //   List
-  // }
+  
 }
 </script>
 
@@ -38,8 +36,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  /* text-align: center; */
   color: #2c3e50;
-  /* margin-top: 60px; */
 }
 </style>
